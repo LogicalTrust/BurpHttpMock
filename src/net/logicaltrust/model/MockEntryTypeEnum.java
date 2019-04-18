c2662ca473e78e75928060c6c9b4a19d51dc5161
Shea Polansky <spolansky@securityevaluators.com>
2019-04-18 16:34:04 -0700

    Fix redirecting to URLs with different paths

--- src/net/logicaltrust/model/MockEntryTypeEnum.java
+++ src/net/logicaltrust/model/MockEntryTypeEnum.java
@@ -3,12 +3,19 @@ import burp.IExtensionHelpers;
 import burp.IHttpRequestResponse;
 import burp.IRequestInfo;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum MockEntryTypeEnum {
     DirectEntry { //traditional one, just returns whatever was entered by the user in the text box
@@ -54,27 +61,23 @@ public enum MockEntryTypeEnum {
                                 !url.getProtocol().equalsIgnoreCase("https"))) {
                     throw new MalformedURLException();
                 }
-            }
-            catch (MalformedURLException e)
-            {
+            } catch (MalformedURLException e) {
                 //TODO: move the logger around so I can access it here
                 request.setComment("Malformed URL! Results may not be what you want...");
                 return false;
             }
             request.setHttpService(helpers.buildHttpService(url.getHost(), port, url.getProtocol()));
             IRequestInfo requestInfo = helpers.analyzeRequest(request);
-            if (requestInfo.getHeaders().stream().anyMatch(s -> s.toLowerCase().startsWith("host: ")))
-            {
-                byte[] body = null;
-                if (requestInfo.getBodyOffset() >= request.getRequest().length || requestInfo.getBodyOffset() < 0)
-                {
-                    body = Arrays.copyOfRange(request.getRequest(),
-                            requestInfo.getBodyOffset(), request.getRequest().length);
-                }
-                request.setRequest(helpers.buildHttpMessage(requestInfo.getHeaders().stream()
-                        .map(s -> s.toLowerCase().startsWith("host: ") ? "Host: " + url.getHost() : s)
-                        .collect(Collectors.toList()), body));
+            byte[] body = null;
+            if (requestInfo.getBodyOffset() >= request.getRequest().length || requestInfo.getBodyOffset() < 0) {
+                body = Arrays.copyOfRange(request.getRequest(),
+                        requestInfo.getBodyOffset(), request.getRequest().length);
             }
+            Stream<String> newTopLine = Stream.of(requestInfo.getMethod() + " " + url.getFile() + " HTTP/1.1");
+            Stream<String> newHeaders = requestInfo.getHeaders().stream()
+                    .map(s -> s.toLowerCase().startsWith("host: ") ? "Host: " + url.getHost() : s).skip(1);
+            request.setRequest(helpers.buildHttpMessage(Stream.concat(newTopLine, newHeaders)
+                    .collect(Collectors.toList()), body));
             return true;
         }
     },
