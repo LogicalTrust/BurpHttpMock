0fa75953fa106ad33e9f9b9a327d62dcc0deb71e
mmmds <michal.dardas@gmail.com>
2022-05-16 16:51:08 +0200

    fix connection errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.1.5
+Fix connection errors.
+
 ## 1.1.4
 Support distinguishing requests by HTTP method.
 
--- src/net/logicaltrust/server/MockLocalServer.java
+++ src/net/logicaltrust/server/MockLocalServer.java
@@ -2,6 +2,10 @@ package net.logicaltrust.server;
 
 import burp.BurpExtender;
 import burp.IExtensionStateListener;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import fi.iki.elonen.NanoHTTPD;
 import net.logicaltrust.SimpleLogger;
 
 import java.io.BufferedOutputStream;
@@ -9,6 +13,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,64 +21,35 @@ public class MockLocalServer implements IExtensionStateListener {
 
     private final SimpleLogger logger;
     private final int port;
-    private boolean stopped = false;
-    private ServerSocket ss;
+    private Server server;
 
     public MockLocalServer(int port) {
         this.logger = BurpExtender.getLogger();
         this.port = port;
     }
 
+    private static class Server extends NanoHTTPD {
+        Server(int port) {
+            super("127.0.0.1", port);
+        }
+    }
+
     public void run() {
         try {
-            ss = new ServerSocket(port, 50, InetAddress.getLoopbackAddress());
-            logger.debugForce("Server has started " + ss);
-            while (!isStopped()) {
-                serve();
-            }
+            server = new Server(port);
+            server.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+            logger.debugForce("Server has started, port=" + port);
         } catch (IOException e) {
             e.printStackTrace(logger.getStderr());
             logger.debugForce("Cannot create server. Try with another port.");
         }
     }
 
-    private void serve() {
-        try {
-            logger.debug("Waiting for connection");
-            handleConnection(ss.accept());
-        } catch (IOException e) {
-            if (isStopped()) {
-                logger.debugForce("Server has stopped");
-            } else {
-                e.printStackTrace(logger.getStderr());
-            }
-        }
-    }
-
-    private void handleConnection(Socket accept) throws IOException {
-        logger.debug("Connection " + accept + " accepted");
-        BufferedReader br = new BufferedReader(new InputStreamReader(accept.getInputStream()));
-        BufferedOutputStream bos = new BufferedOutputStream(accept.getOutputStream());
-        bos.write("HTTP/1.0 292 Mock\r\nContent-Type: application/x\r\n\r\n".getBytes());
-        bos.close();
-        br.close();
-        accept.close();
-    }
-
-    private synchronized boolean isStopped() {
-        return stopped;
-    }
-
-    private synchronized void setStopped(boolean stopped) {
-        this.stopped = stopped;
-    }
-
     @Override
     public void extensionUnloaded() {
-        setStopped(true);
         try {
-            ss.close();
-        } catch (IOException e) {
+            server.stop();
+        } catch (Exception e) {
             e.printStackTrace(logger.getStderr());
         }
     }
